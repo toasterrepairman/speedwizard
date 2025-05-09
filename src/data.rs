159e1b083ed8d18831b79bb6b7bdc72e9034fc6c
Joel B. <smol@toast.cyou>
2025-05-09 02:14:54 -0400

    data

---
+++ src/data.rs
@@ -0,0 +1,30 @@
+pub const CSV_DATA: &str = r#"system,object_class,name,atmospheric_pressure
+Stanton,Planet,Hurston,0.89
+Stanton,Moon,Arial,0.77
+Stanton,Moon,Aberdeen,1.04
+Stanton,Moon,Magda,0.33
+Stanton,Moon,Ita,0.15
+Stanton,Planet,Crusader,1.0
+Stanton,Moon,Cellin,0.19
+Stanton,Moon,Daymar,0.6
+Stanton,Moon,Yela,0.19
+Stanton,Planet,ArcCorp,0.84
+Stanton,Moon,Lyria,0.09
+Stanton,Moon,Wala,0.1
+Stanton,Planet,microTech,1.1
+Stanton,Moon,Calliope,0.8
+Stanton,Moon,Clio,0.62
+Stanton,Moon,Euterpe,0.79
+Pyro,Planet,Pyro I,2.3824
+Pyro,Planet,Monox,0.908
+Pyro,Planet,Bloom,0.9889
+Pyro,Planet,Pyro V,2.9854
+Pyro,Moon,Ignis,0.077
+Pyro,Moon,Vatra,1.207
+Pyro,Moon,Adir,0.0493
+Pyro,Moon,Fairo,1.7705
+Pyro,Moon,Fuego,0.5606
+Pyro,Moon,Vuur,1.2544
+Pyro,Protoplanet,Pyro IV,0.4875
+Pyro,Planet,Terminus,1.128
+"#;
